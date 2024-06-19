5057a0d7338221c3d77c0158a57bfc44b08d7718
Sama <etornamasamoah@gmail.com>
2024-06-19 08:59:29 +0000

    Logistics - Inventory-5

--- tas_project/urls.py
+++ tas_project/urls.py
@@ -1,4 +1,7 @@
 
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,3 +31,8 @@ urlpatterns = [
     path('api/token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
